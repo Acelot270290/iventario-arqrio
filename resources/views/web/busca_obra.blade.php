742f119f10461c758e408559d569f122f0a28112
Alan <you@example.com>
2022-11-29 21:18:08 -0300

    Acrescentando Gif

--- resources/views/web/busca_obra.blade.php
+++ resources/views/web/busca_obra.blade.php
@@ -4,6 +4,54 @@
 
 @section('content')
 
+<style>
+
+body {
+  background: #ececec;
+}
+.lds-dual-ring.hidden { 
+display: none;
+}
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 5% auto;
+  border-radius: 50%;
+  border: 6px solid #fff;
+  border-color: #fff transparent #fff transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background: rgba(0,0,0,.8);
+    z-index: 999;
+    opacity: 1;
+    transition: all 0.5s;
+}
+
+  </style>
+
 <div class="busca-obras-publico" style="min-height: 562px;">
   <section class="section">
     <div class="section-body">
@@ -267,6 +315,7 @@
               </div>
             </div>
         </div>
+        <div id="loader" class="lds-dual-ring hidden overlay"></div>
         <!-- Home da área restrita -->
       </div>
     </div>
@@ -277,6 +326,7 @@
   $(document).on('submit', '#busca_form', function(e) {
       e.preventDefault();
       let id = $(this).attr('data-id');
+      $('#loader').removeClass('hidden')
       $.ajax({
           url: '{{ route("busca_obras_publico_form") }}',
           type: 'POST',
@@ -284,8 +334,10 @@
             'X-CSRF-TOKEN': $('input[name=_token]').val()
           },
           data: $(this).serialize(),
+          
       }).done(function(data) {
           console.log(data);
+          $('#loader').addClass('hidden')
           $('#buscaObras').empty().append(data);
       });
   });
